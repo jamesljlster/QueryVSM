ddd1ccadd876a83a38b93a509005986aa277d3eb
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-06-09 17:26:25 +0800

    Add DocInfo class

--- QueryVSM/DataPrep/DataPrep.cs
+++ QueryVSM/DataPrep/DataPrep.cs
@@ -4,11 +4,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml;
+using System.Net;
 
 namespace QueryVSM
 {
+    class DocInfo
+    {
+        // Constructor
+        public DocInfo()
+        {
+            this.title = null;
+            this.abstruct = null;
+            this.authorInfo = null;
+        }
+
+        public override string ToString()
+        {
+            String tmp = "";
+
+            if(this.title != null)
+            {
+                tmp += "Title: " + this.title + "\r\n";
+            }
+            
+            if(this.authorInfo != null)
+            {
+                tmp += "Author Info: " + this.title + "\r\n";
+            }
+
+            if(this.abstruct != null)
+            {
+                tmp += "Abstruct: " + this.abstruct + "\r\n";
+            }
+
+            return tmp;
+        }
+
+        public String title;
+        public String abstruct;
+        public String authorInfo;
+    }
+
     class DataPrep
     {
-        
+        // Function: Parse xml documents to document list
+        public static List<DocInfo> parse_xml_docs(String xmlDoc)
+        {
+            // Create xml
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(xmlDoc);
+
+            // Parsing xml
+            XmlNodeList bookArticle = xml.SelectNodes("PubmedBookArticle");
+            XmlNodeList journalArticl = xml.SelectNodes("PubmedArticle");
+
+            return null;
+        }
+
+        // Function: Get documents associated with query term
+        public static String request_xml_docs(String queryTerm, int retMax, int relDate)
+        {
+            String url;
+            String webEnv;
+
+            StreamReader reader;
+
+            XmlDocument xml;
+            XmlNode xmlNode;
+
+            // Generate web environment request url
+            url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&datetype=edat&usehistory=y" +
+                "&term=" + queryTerm +
+                "&reldate=" + relDate.ToString() +
+                "&retmax=" + retMax.ToString();
+
+            // Processing request and response
+            reader = new StreamReader(WebRequest.Create(url).GetResponse().GetResponseStream());
+
+            // Parsing web environment key
+            xml = new XmlDocument();
+            xml.LoadXml(reader.ReadToEnd());
+            xmlNode = xml.SelectSingleNode("eSearchResult/WebEnv");
+            if (xmlNode == null)
+            {
+                return null;
+            }
+
+            webEnv = xmlNode.InnerText;
+
+            // Generate paper request url
+            url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract&query_key=1&restart=0" +
+                "&term=" + queryTerm +
+                "&retmax=" + retMax.ToString() +
+                "&WebEnv=" + webEnv;
+
+            // Get papers
+            reader = new StreamReader(WebRequest.Create(url).GetResponse().GetResponseStream());
+
+            return reader.ReadToEnd();
+        }
     }
 }
--- QueryVSM/Form1.cs
+++ QueryVSM/Form1.cs
@@ -15,21 +15,7 @@ namespace QueryVSM
         public Form1()
         {
             InitializeComponent();
-
-            VectorSpaceModel vsm = new VectorSpaceModel();
-            vsm.add_doc("Users need to translate their information needs into queries and submit into IR systems.The key goal of an IR system is to retrieve all the items that are relevant to a user query, while retrieving as few nonrelevant items as possible.");
-            vsm.add_doc("Possible approaches to effectively and efficiently searching or finding relevant documents.Ranking scores all retrieved documents according to a relevance metric.");
-            vsm.add_doc("Initial exploration of text retrieval systems for “small” corpora of scientific abstracts, and law and business documents.IR systems have filters to handle most popular documents.");
-            vsm.add_doc("Strings of the same length, distance between them is the number of positions with different characters.The definition of a ranking function that allows quantifying the similarities among documents and queries.");
-
-            String[] rankedDoc = vsm.get_ranked_doc("documents queries users");
-
-            for(int i = 0; i < rankedDoc.Length; i++)
-            {
-                debugMsg.Text += "Rank " + i.ToString() + "\r\n";
-                debugMsg.Text += rankedDoc[i] + "\r\n";
-                debugMsg.Text += "\r\n";
-            }
+            debugMsg.Text = DataPrep.request_docs_xml("AIDS", 100, 60);
         }
     }
 }
